f1db63e398ec57f013314612f6cda891fa62b137
Mithun RS <mithunsivanantham183@gmail.com>
2025-02-14 13:36:09 +0530

    first commit

--- react2/src/component/Navbar.jsx
+++ react2/src/component/Navbar.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import {Link } from 'react-router-dom'
 import "./Navbar.css"
 import { useNavigate } from 'react-router-dom';
-const navigate = useNavigate();
+
 
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <nav>
-  <h1 className="title" onClick={() => navigate("/")}>FOODIE</h1>         <ul>
+  <h1 className="title" onClick={() => navigate("/")}>FOODIE</h1>  
+         <ul>
             <li>
             <Link to="/about">About</Link>
             </li>
